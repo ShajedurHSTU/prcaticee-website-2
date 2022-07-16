999247925488b33b7674879aeafbc2aec21660ad
ShajedurHSTU <shajedur.shuvo007@gmail.com>
2022-07-16 10:28:36 +0600

    first ever commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# prcaticee-website-2
---
+++ pages/hello.js
@@ -0,0 +1,11 @@
+var number1=10;
+var number2=20;
+var total=number1+number2;
+console.log("the summation of two number is "+ total);
+var difference= number2-number1
+console.log("the difference of two number is "+ difference)
+for(i=0; i<10; i++)
+{
+    console.log(i);
+}
+console.log(typeof total)
